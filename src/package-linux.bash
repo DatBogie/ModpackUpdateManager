a97e9de337533e47c97743f1890426117d8fb3da
DatBogie <datbogie@datbogie.org>
2026-08-12 00:28:47 -0400

    Setup packing script for Linux \(Build on Debian 12\!\)

--- src/package-linux.bash
+++ src/package-linux.bash
@@ -12,8 +12,33 @@ REPO_ROOT_DIR="$PWD" &&
 	echo "Copying binary to $PWD/dist..." &&
 	cp appsrc dist/ModpackUpdateManager &&
 	cd dist &&
-	printf "[Desktop Entry]\nType=Application\nName=Modpack Update Manager\nExec=ModpackUpdateManager\nCategories=Utility;" > ../ModpackUpdateManager.desktop &&
+	echo "Finding latest Qt location..." &&
+	latestQtVersion=$(
+		for dir in ~/Qt/*/; do
+			name=${dir%/}
+			name=${name##*/}
+			[[ $name =~ ^[0-9]+(\.[0-9]+){1,3}$ ]] && echo "$name"
+		done |
+		awk -F. '{ printf "%010d.%010d.%010d %s\n", $1, $2, $3, $0 }' |
+		sort |
+		tail -n 1 |
+		cut -d' ' -f2
+	) &&
+	buildSystem=$(
+		for dir in ~/Qt/"$latestQtVersion"/*/; do
+			name=${dir%/}
+			name=${name##*/}
+			[[ -x "$dir/bin/qmake" ]] && echo "$name"
+			break
+		done
+	) &&
+	echo "Using Qt $latestQtVersion with $buildSystem" &&
+	echo "Copying .desktop file into $PWD..." &&
+	cp "$REPO_ROOT_DIR/.ModpackUpdateManager.desktop" ./ModpackUpdateManager.desktop &&
+	echo "Copying icon svg into $PWD..." &&
+	cp "$REPO_ROOT_DIR/assets/icon.svg" ./icon.svg &&
+	export QMAKE="$HOME/Qt/$latestQtVersion/$buildSystem/bin/qmake" &&
 	export QML_SOURCES_PATHS="$REPO_ROOT_DIR" &&
 	echo "Bundling Qt/QML library files via linuxdeploy/linuxdeploy-plugin-qt..." &&
-	linuxdeploy --appdir AppDir -e ModpackUpdateManager -d ../ModpackUpdateManager.desktop --plugin qt --output appimage &&
-	echo "Done!"
+	linuxdeploy --appdir AppDir -d ModpackUpdateManager.desktop -e ModpackUpdateManager -i icon.svg --plugin qt --output appimage &&
+	echo "Done!" || echo "Failed!"
